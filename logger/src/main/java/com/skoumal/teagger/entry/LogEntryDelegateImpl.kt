d009cdd1eb8abd3c996fdb7c9324590af4fe7e41
Viktor De Pasquale <info@depasquale.wiki>
2020-03-16 16:37:45 +0100

    Added date to all log entries

--- logger/src/main/java/com/skoumal/teagger/entry/LogEntryDelegateImpl.kt
+++ logger/src/main/java/com/skoumal/teagger/entry/LogEntryDelegateImpl.kt
@@ -2,8 +2,12 @@ package com.skoumal.teagger.entry
 
 import android.util.Log
 import com.skoumal.teagger.Constants
+import java.text.DateFormat
+import java.util.*
 
 internal class LogEntryDelegateImpl : LogEntryDelegate {
+
+    private val format = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM)
 
     override fun entryFor(priority: Int, tag: String, message: String): String {
         val priorityString = when (priority) {
@@ -16,11 +20,11 @@ internal class LogEntryDelegateImpl : LogEntryDelegate {
             else -> ""
         }
 
-        return "$priorityString/$tag: ${message.ifBlank { "{Blank error message}" }}"
+        return "${format.format(Date())} - $priorityString/$tag: ${message.ifBlank { "{Blank error message}" }}"
     }
 
     override fun entryFor(throwable: Throwable): String {
-        return "E/${throwable.message ?: Constants.CRASH_DEFAULT_MESSAGE}"
+        return "${format.format(Date())} - E/${throwable.message ?: Constants.CRASH_DEFAULT_MESSAGE}"
     }
 
 }
