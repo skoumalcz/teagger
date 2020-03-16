01d7bc14d9971d409ca693ce1b97868b4edc22fe
Viktor De Pasquale <info@depasquale.wiki>
2020-03-16 13:08:39 +0100

    Removed deprecated members

--- logger/src/main/java/com/skoumal/teagger/crash/StreamCrashHandler.kt
+++ logger/src/main/java/com/skoumal/teagger/crash/StreamCrashHandler.kt
@@ -1,35 +1,10 @@
 package com.skoumal.teagger.crash
 
-import android.content.Context
-import com.skoumal.teagger.Constants
 import com.skoumal.teagger.StreamLogger
-import com.skoumal.teagger.setFile
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.io.File
-import java.io.IOException
 
 internal class StreamCrashHandler(
     private val logger: StreamLogger
 ) {
-
-    @Deprecated("Class needs to be provided with a logger. Creating it by itself defies the contract that logger makes with the crash handler")
-    constructor(logger: StreamLogger, context: Context) : this(logger) {
-        try {
-            val dir = File(
-                context.filesDir,
-                Constants.FILES_DIR
-            ).apply { mkdirs() }
-            val file = File(dir, Constants.CRASH_LOG_FILE).apply { createNewFile() }
-            logger.setFile(file)
-        } catch (t: Throwable) {
-            if (t is IOException || t is SecurityException) {
-                t.printStackTrace()
-            } else {
-                throw t
-            }
-        }
-    }
 
     init {
         val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
@@ -37,18 +12,6 @@ internal class StreamCrashHandler(
             // todo dump the thread group
             handleCrash(throwable)
             defaultHandler?.uncaughtException(thread, throwable)
-        }
-    }
-
-    /**
-     * Call this method in your Application class in order to log any crashes that failed to log
-     * to the desired stream.
-     */
-    @Deprecated("This should not be a responsibility of the crash handler.")
-    fun pushToLog() {
-        GlobalScope.launch {
-            //logger.log(fileLogger.getLogAsString(), null)
-            //logger.wipeLog()
         }
     }
 
